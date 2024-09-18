9d1ab3982db3e7b83561c7c9d650309d31688122
soching <chunsoching@gmail.com>
2024-09-18 11:20:24 +0700

    here is my code model on supplier

---
+++ testGit/Models/supplier.cs
@@ -0,0 +1,10 @@
+﻿namespace testGit.Models
+{
+    public class supplier
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+    }
+}
