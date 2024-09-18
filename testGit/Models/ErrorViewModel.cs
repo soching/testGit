4570f4816157529549efe717b4a9c8079e9d3063
soching <chunsoching@gmail.com>
2024-09-18 10:09:18 +0700

    Add project files.

---
+++ testGit/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace testGit.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
